b26358e6f011807b1dc12b524721071d7132680b
mlshvch <malashevichdev@gmail.com>
2021-10-06 14:12:27 +0300

    6-th task done without recursion

--- itechart_tasks.rb
+++ itechart_tasks.rb
@@ -24,27 +24,50 @@ def phone_number(string)
     pp string.match?(/^\+375\((17|29|33|44|25)\)[\d]{7}$/)
 end
 
-times = [['10:00', '10:20'], ['10:40', '11:00'], ["10:50", "12:00"], ["12:00", "13:00"], ['10:00', '10:20']]
+times = [['10:00', '10:20'], ['13:00', '15:00'], ['10:40', '11:00'], ["10:50", "12:00"], ["12:00", "13:00"], ['10:00', '10:20']]
 
-def merge_time(times)
-    times = times.uniq.sort
-    i = 1
-    while i > 0 do
-
-        if times[i - 1][1] >= times[i][0] then
-            times[i - 1] += times[i]
-        end
-
-        i += 1
-
-        pp times
-
-        if i == times.length then
-            i = 1
-        end
-        times = times.uniq
+def intersect(array1, array2)
+    if array1[1] >= array2[0] then
+        return true
+    else
+        return false
     end
-
 end
 
-substr("a b c a d d d b, d")
+def merge(array1, array2)
+    if array1[1] >= array2[0] then
+        array1 += array2
+        return [array1.min, array1.max]
+    else
+        return [array1, array2]
+    end
+end
+
+def merge_time(times)
+    times = times.sort.uniq 
+    count = times.length + 1
+    while times.length > 1 && count > 0 do
+        i = 1
+        while i < times.length - 1 do
+            if intersect(times[-i - 1], times[-i]) then
+                times[-i - 1] = merge(times[-i - 1], times[-i])
+                times.delete_at(-i)
+                i += 2
+                count += 1
+            else 
+                i += 1
+                count -= 1
+            end
+        end
+
+        if times.length == 2 then
+            pp merge(times[0], times[1])
+            return 
+        end
+
+    end
+    pp times
+end
+
+merge_time(times)
+
